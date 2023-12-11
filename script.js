3553d0d6d9fdc71032b641493afc5d75db7655da
quandaworld <46980777+quandaworld@users.noreply.github.com>
2023-12-11 13:35:11 -0800

    Update addMark func to include updating DOM for each turn

--- script.js
+++ script.js
@@ -6,13 +6,21 @@ cell_div.forEach(cell => cell.addEventListener('click', addMark, {once: true}));
 
 restart_btn.addEventListener('click', () => {
   location.reload();
-})
+});
 
 function addMark(e) {
   if (xTurn) {
     e.target.classList.add('x');
+    document.getElementById('o-player').classList.add('o');
+    document.getElementById('x-player').classList.remove('x');
+    document.querySelector('.game-board').classList.remove('x');
+    document.querySelector('.game-board').classList.add('o');
   } else {
     e.target.classList.add('o');
+    document.getElementById('o-player').classList.remove('o');
+    document.getElementById('x-player').classList.add('x');
+    document.querySelector('.game-board').classList.add('x');
+    document.querySelector('.game-board').classList.remove('o');
   }
   xTurn = !xTurn;
 }
