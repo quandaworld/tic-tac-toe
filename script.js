ff3d3e589824c5cc0bca23b3ef5ef86446ae3322
quandaworld <46980777+quandaworld@users.noreply.github.com>
2023-12-11 23:08:33 -0800

    Refactor code
    - Encapsulate the game logic within the Game module
    - Create variables to store DOM elements
    - Create callback functions to add marks, highlight current player, and set board hover class
    - Store all event listeners in startGame function
    - Invoke startGame function

--- script.js
+++ script.js
@@ -1,34 +1,71 @@
-let mode = '';
-
-window.addEventListener('load', () => {
-  document.querySelector('.mode').classList.add('show');
-});
-
-document.querySelectorAll('.mode-btns button').forEach(btn => btn.addEventListener('click', (e) => {
-  mode = e.target.dataset.mode;
-  document.querySelector('.mode').classList.remove('show');
-}));
-
-document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', addMark, {once: true}));
-
-document.getElementById('restart').addEventListener('click', () => {
-  location.reload();
-});
-
-function addMark(e) {
+const Game = (function() {
+  const X_PLAYER = 'x';
+  const O_PLAYER = 'o';
+  const oScoreBoard = document.getElementById('o-player');
+  const xScoreBoard = document.getElementById('x-player');
+  const gameBoard = document.querySelector('.game-board');
+  const mode_div = document.querySelector('.mode');
+  const mode_btns = document.querySelectorAll('.mode-btns button');
+  const cell_divs = document.querySelectorAll('.cell');
+  const restart_btn = document.getElementById('restart');
+  let mode = '';
   let xTurn = true;
-  if (xTurn) {
-    e.target.classList.add('x');
-    document.getElementById('o-player').classList.add('o');
-    document.getElementById('x-player').classList.remove('x');
-    document.querySelector('.game-board').classList.remove('x');
-    document.querySelector('.game-board').classList.add('o');
-  } else {
-    e.target.classList.add('o');
-    document.getElementById('o-player').classList.remove('o');
-    document.getElementById('x-player').classList.add('x');
-    document.querySelector('.game-board').classList.add('x');
-    document.querySelector('.game-board').classList.remove('o');
+
+  function addMark(e, currentPlayer) {
+    e.target.classList.add(currentPlayer);
   }
-  xTurn = !xTurn;
-}
+
+  function highlightCurrentPlayer() {
+    oScoreBoard.classList.remove(O_PLAYER);
+    xScoreBoard.classList.remove(X_PLAYER);
+    if (xTurn) {
+      oScoreBoard.classList.add(O_PLAYER);
+    } else {
+      xScoreBoard.classList.add(X_PLAYER);
+    }
+  }
+
+  function setBoardHoverClass() {
+    gameBoard.classList.remove(O_PLAYER);
+    gameBoard.classList.remove(X_PLAYER);
+    if (xTurn) {
+      gameBoard.classList.add(O_PLAYER);
+    } else {
+      gameBoard.classList.add(X_PLAYER);
+    }
+  }
+
+  function handleClick(e) {
+    const currentPlayer = xTurn ? X_PLAYER : O_PLAYER;
+    addMark(e, currentPlayer);
+    highlightCurrentPlayer();
+    setBoardHoverClass();
+
+    // check for win
+    // check for draw
+    xTurn = !xTurn;
+  }
+  
+  function startGame() {
+    mode_div.classList.add('show');
+
+    mode_btns.forEach(btn => {
+      btn.addEventListener('click', (e) => {
+        mode = e.target.dataset.mode;
+        document.querySelector('.mode').classList.remove('show');
+      });
+    });
+
+    cell_divs.forEach(cell => {
+      cell.addEventListener('click', handleClick, {once: true});
+    });
+
+    restart_btn.addEventListener('click', () => {
+      location.reload();
+    });
+  }
+
+  return {startGame};
+})();
+
+Game.startGame();
