371e27d8096e080a61dd239a92d99073cbbf9a77
quandaworld <46980777+quandaworld@users.noreply.github.com>
2023-12-12 18:39:02 -0800

    Split the code to two modules
    - One module for DOM logic, and one for game flow
    - Store the winning combinations in an array
    - Create function to update game board after each turn

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-const Game = (function() {
+const DOMLogic = (function() {
   const X_PLAYER = 'x';
   const O_PLAYER = 'o';
   const oScoreBoard = document.getElementById('o-player');
   const xScoreBoard = document.getElementById('x-player');
-  const gameBoard = document.querySelector('.game-board');
+  const gameBoard_div = document.querySelector('.game-board');
   const mode_div = document.querySelector('.mode');
   const mode_btns = document.querySelectorAll('.mode-btns button');
-  const cell_divs = document.querySelectorAll('.cell');
   const restart_btn = document.getElementById('restart');
-  let mode = '';
+  const cell_divs = document.querySelectorAll('.cell');
   let xTurn = true;
+  let currentPlayer;
+  let mode;
+
+  mode_div.classList.add('show');
+
+  mode_btns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      mode = e.target.dataset.mode;
+      document.querySelector('.mode').classList.remove('show');
+    });
+  });
+
+  restart_btn.addEventListener('click', () => {
+    location.reload();
+  });
+
+  cell_divs.forEach(cell => {
+    cell.addEventListener('click', handleClick, {once: true});
+  });
 
   function addMark(e, currentPlayer) {
     e.target.classList.add(currentPlayer);
@@ -26,46 +44,56 @@ const Game = (function() {
   }
 
   function setBoardHoverClass() {
-    gameBoard.classList.remove(O_PLAYER);
-    gameBoard.classList.remove(X_PLAYER);
+    gameBoard_div.classList.remove(O_PLAYER);
+    gameBoard_div.classList.remove(X_PLAYER);
     if (xTurn) {
-      gameBoard.classList.add(O_PLAYER);
+      gameBoard_div.classList.add(O_PLAYER);
     } else {
-      gameBoard.classList.add(X_PLAYER);
+      gameBoard_div.classList.add(X_PLAYER);
     }
   }
 
   function handleClick(e) {
-    const currentPlayer = xTurn ? X_PLAYER : O_PLAYER;
+    currentPlayer = xTurn ? X_PLAYER : O_PLAYER;
     addMark(e, currentPlayer);
     highlightCurrentPlayer();
     setBoardHoverClass();
-
-    // check for win
-    // check for draw
+    game.updateGameBoard(e);
+    game.checkForWin();
+    game.checkForTie();
     xTurn = !xTurn;
   }
-  
-  function startGame() {
-    mode_div.classList.add('show');
 
-    mode_btns.forEach(btn => {
-      btn.addEventListener('click', (e) => {
-        mode = e.target.dataset.mode;
-        document.querySelector('.mode').classList.remove('show');
-      });
-    });
-
-    cell_divs.forEach(cell => {
-      cell.addEventListener('click', handleClick, {once: true});
-    });
-
-    restart_btn.addEventListener('click', () => {
-      location.reload();
-    });
-  }
-
-  return {startGame};
+  return {
+    getCurrentPlayer: () => currentPlayer,
+    getMode: () => mode,
+  };
 })();
 
-Game.startGame();
+const game = (function() {
+  const gameBoardArr = ['', '', '', '', '', '', '', '', ''];
+  const winCombinations = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8]
+  ];
+
+  function updateGameBoard(e) {
+    gameBoardArr.splice(e.target.dataset.cell, 1, DOMLogic.getCurrentPlayer());
+  }
+
+  function checkForWin() {
+
+  }
+
+  function checkForTie() {
+
+  }
+
+  return {updateGameBoard, checkForWin, checkForTie};
+})();
